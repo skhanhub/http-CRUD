2a72fdf890d7f3e9e03e518a3494ee8cd4dc6392
hasankhs <shahriar.hasankhan@anz.com>
2021-06-29 22:25:30 +1000

    mock product data

---
+++ src/models/__mocks__/productMockData.ts
@@ -0,0 +1,16 @@
+export default [
+	{
+		"id": "8F2E9176-35EE-4F0A-AE55-83023D2DB1A3",
+		"name": "Samsung Galaxy S7",
+		"description": "Newest mobile product from Samsung.",
+		"price": 1024.99,
+		"deliveryPrice": 16.99
+	},
+	{
+		"id": "DE1287C0-4B15-4A7B-9D8A-DD21B3CAFEC3",
+		"name": "Apple iPhone 6S",
+		"description": "Newest mobile product from Apple.",
+		"price": 1299.99,
+		"deliveryPrice": 15.99
+	}
+]
