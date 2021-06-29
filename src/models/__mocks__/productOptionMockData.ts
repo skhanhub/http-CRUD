59d10ced17b5ba57921eb04f0eb603e6e8fa0f5f
hasankhs <shahriar.hasankhan@anz.com>
2021-06-29 22:24:12 +1000

    ProductOptions mock data

---
+++ src/models/__mocks__/productOptionMockData.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    "id": "5C2996AB-54AD-4999-92D2-89245682D534",
+    "productId": "DE1287C0-4B15-4A7B-9D8A-DD21B3CAFEC3",
+    "name": "Rose Gold",
+    "description": "Gold Apple iPhone 6S"
+  },
+  {
+    "id": "9AE6F477-A010-4EC9-B6A8-92A85D6C5F03",
+    "productId": "DE1287C0-4B15-4A7B-9D8A-DD21B3CAFEC3",
+    "name": "White",
+    "description": "White Apple iPhone 6S"
+  },
+  {
+    "id": "4E2BC5F2-699A-4C42-802E-CE4B4D2AC0EF",
+    "productId": "DE1287C0-4B15-4A7B-9D8A-DD21B3CAFEC3",
+    "name": "Black",
+    "description": "Black Apple iPhone 6S"
+  },
+  {
+    "id": "0643CCF0-AB00-4862-B3C5-40E2731ABCC9",
+    "productId": "8F2E9176-35EE-4F0A-AE55-83023D2DB1A3",
+    "name": "White",
+    "description": "White Samsung Galaxy S7"
+  },
+  {
+    "id": "A21D5777-A655-4020-B431-624BB331E9A2",
+    "productId": "8F2E9176-35EE-4F0A-AE55-83023D2DB1A3",
+    "name": "Black",
+    "description": "Black Samsung Galaxy S7"
+  }
+]
